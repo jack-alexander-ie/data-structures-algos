87c2048ea7a2f982daf49e74e00560f02f57b398
Jack Mason <jackmasonmusic92@gmail.com>
2019-06-28 13:24:51 -0700

    Starting union and intersection problem

--- Projects/Project_2/problem_6.py
+++ Projects/Project_2/problem_6.py
@@ -1,15 +1,15 @@
 """
 Union and Intersection of Two Linked Lists
 
-Your task for this problem is to fill out the union and intersection functions. The union of two sets A and B is the
-set of elements which are in A, in B, or in both A and B. The intersection of two sets A and B, denoted by A ∩ B, is
-the set of all objects that are members of both the sets A and B.
+Fill out the union and intersection functions.
 
-You will take in two linked lists and return a linked list that is composed of either the union or intersection,
-respectively. Once you have completed the problem you will create your own test cases and perform your own run time
-analysis on the code.
+The union of two sets is the set of elements which are in the 1st list, in the 2nd list, or in both.
+The intersection of two sets is the set of all objects that are members of both the sets A and B.
+
+Take in two linked lists and return a linked list that is composed of either the union or intersection.
 
 TODO: Test cases
+TODO: Runtime analysis
 """
 
 
@@ -56,12 +56,12 @@ class LinkedList:
         return size
 
 
-def union(llist_1, llist_2):
+def union(list_1: LinkedList, list_2: LinkedList):
     # Your Solution Here
     pass
 
 
-def intersection(llist_1, llist_2):
+def intersection(list_1: LinkedList, list_2: LinkedList):
     # Your Solution Here
     pass
 
@@ -71,8 +71,8 @@ def intersection(llist_1, llist_2):
 linked_list_1 = LinkedList()
 linked_list_2 = LinkedList()
 
-element_1 = [3,2,4,35,6,65,6,4,3,21]
-element_2 = [6,32,4,9,6,1,11,21,1]
+element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
+element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
 
 for i in element_1:
     linked_list_1.append(i)
@@ -80,16 +80,16 @@ for i in element_1:
 for i in element_2:
     linked_list_2.append(i)
 
-print (union(linked_list_1,linked_list_2))
-print (intersection(linked_list_1,linked_list_2))
+print(union(linked_list_1, linked_list_2))
+print(intersection(linked_list_1, linked_list_2))
 
 # Test case 2
 
 linked_list_3 = LinkedList()
 linked_list_4 = LinkedList()
 
-element_1 = [3,2,4,35,6,65,6,4,3,23]
-element_2 = [1,7,8,9,11,21,1]
+element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]
+element_2 = [1, 7, 8, 9, 11, 21, 1]
 
 for i in element_1:
     linked_list_3.append(i)
@@ -97,5 +97,5 @@ for i in element_1:
 for i in element_2:
     linked_list_4.append(i)
 
-print (union(linked_list_3,linked_list_4))
-print (intersection(linked_list_3,linked_list_4))
+print(union(linked_list_3, linked_list_4))
+print(intersection(linked_list_3, linked_list_4))
