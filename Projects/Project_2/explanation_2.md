17af6cf34a9a639a212dcf127a38787eb452a84a
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-02 17:35:46 -0700

    Problem 2 explanation added

---
+++ Projects/Project_2/explanation_2.md
@@ -0,0 +1,19 @@
+# Problem 2 - File Recursion
+
+The only data structure used in this solution is a list which is used to collect the various filepaths for matching files.
+
+A list was selected for it's simplicity:
+
+    1. Sets - a list of unique files is not needed
+    2. Dicts - only the file names are needed, so no key/value pairs
+    3. Tuples - are immutable so paths cannot be added
+    4. Strings - could be used but looks untidy and does not offer versatility for potential later use
+    5. Linked lists - could be considered to be overkill, even though they'd save on the computational time spent doubling an array
+
+### Time Complexity
+
+The time complexity for this solution could be considered O(n) as it depends on the number of matching fies contained in a folder.
+
+### Space Complexity
+
+Similarly, the space complexity for this solution could be considered O(n) as it depends on the number of matching fies contained in a folder.
--- Projects/Project_2/problem_2.py
+++ Projects/Project_2/problem_2.py
@@ -1,15 +1,22 @@
 import os
 
 
-def find_files(suffix, path):
-
+def validity_checker(suffix: str, path: str) -> tuple:
     # Check path path validity
     if not os.path.exists(path) or not os.access(os.path.dirname(path), os.W_OK):
-        return 'The path given is invalid'
-
+        return False, 'The path given is invalid'
     # Check suffix validity
     if suffix is '' or not suffix.startswith('.') or len(suffix) < 2:
-        return 'The suffix given is invalid'
+        return False, 'The suffix given is invalid'
+    return True, None
+
+
+def find_files(suffix: str, path: str):
+
+    # Check path and suffix validity
+    validity, message = validity_checker(suffix, path)
+    if not validity:
+        return message
 
     # Create array to store results
     files = []
@@ -17,21 +24,14 @@ def find_files(suffix, path):
     # Search directories recursively
     def find_files_rec(suffix, path, files):
         """ Code referenced from: https://www.devdungeon.com/content/walk-directory-python """
-
         path = os.path.abspath(path)
-
         for item in os.listdir(path):
-
             item_full_path = os.path.join(path, item)
-
             if os.path.isdir(item_full_path):
                 find_files_rec(suffix, item_full_path, files)
-
             elif item_full_path.endswith(suffix):
                 files.append(item_full_path)
-
     find_files_rec(suffix, path, files)
-
     # Only return results if results found
     if len(files) > 0:
         return files
@@ -53,7 +53,7 @@ Expected Output:
 Expected Output: 'The path given is invalid'
 """
 
-# Test Case 3 - invalid suffix
+# Test Case 3 - Invalid suffix
 # suffix, path = '.', 'testdir/'
 """
 Expected Output: 'The suffix given is invalid'
