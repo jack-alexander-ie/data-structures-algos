e8d079e42347197594e7f3047e54a555122d29b8
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-05 15:07:05 -0700

    Changed square root function from recursive to iterative

--- Projects/Project_3/problem_1.py
+++ Projects/Project_3/problem_1.py
@@ -1,34 +1,32 @@
-import time
+def sqrt(number):
 
-
-def sqrt(number, comparator=-1, prev_mult=0):
-
-    # Test block for initial call
-    if comparator == -1:
-        comparator = number
-
-    if comparator == 0 or comparator == 1:              # No root, return number itself
+    if number == 0 or number == 1:                      # No root, return number itself
         return number                                   # Return itself as only square
 
-    center_value = (comparator - 1) // 2                # Center value
-    center_multiplied = center_value * center_value
+    start, end, root = 1, number, 0
 
-    if center_multiplied == number:                     # Perfect square exists, return value
-        return center_value
-    elif center_multiplied < number:                    # Move right
+    while start <= end:
 
-        prev_square = prev_mult * prev_mult             # Get square of previous multiplier
-        if prev_square > number:                        # If previous square > target..
-            return center_value                         # ..return floored value
+        center_value = (start + end) // 2  # Center value
+        center_multiplied = center_value * center_value
 
-        return sqrt(number, comparator + 2, center_value)
-    else:                                               # Move left
-        return sqrt(number, comparator - 2, center_value)
+        if center_multiplied == number:                     # Perfect square exists, return value
+            return center_value
+        elif center_multiplied < number:                    # Move right
+            start = center_value + 1
+            root = center_value
+        else:                                               # Move left
+            end = center_value - 1
+
+    return root
+
+# Test Cases - All pass
+# print("Pass" if (3 == sqrt_rec(9)) else "Fail")
+# print("Pass" if (0 == sqrt_rec(0)) else "Fail")
+# print("Pass" if (4 == sqrt_rec(16)) else "Fail")
+# print("Pass" if (1 == sqrt_rec(1)) else "Fail")
+# print("Pass" if (5 == sqrt_rec(27)) else "Fail")
 
 
-# Test Cases
-print("Pass" if (3 == sqrt(9)) else "Fail")
-print("Pass" if (0 == sqrt(0)) else "Fail")
-print("Pass" if (4 == sqrt(16)) else "Fail")
-print("Pass" if (1 == sqrt(1)) else "Fail")
-print("Pass" if (5 == sqrt(27)) else "Fail")
+# Test Case - Large numbers
+# print("Pass" if (100 == sqrt_iter(10000)) else "Fail")
