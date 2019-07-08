e8e3e293bd518c0de8a58bdb72b3a0915794c69b
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-08 13:30:50 -0700

    Beginning work on the case sort merge implementation

---
+++ Topics/3. Basic Algorithms/2. Sorting Algorithms/merge_sort_string_sort.py
@@ -0,0 +1,36 @@
+def case_sort(string):
+    """
+    Here are some pointers on how the function should work:
+    1. Sort the string
+    2. Create an empty output list
+    3. Iterate over original string
+        if the character is lower-case:
+            pick lower-case character from sorted string to place in output list
+        else:
+            pick upper-case character from sorted string to place in output list
+
+    Note: You can use Python's inbuilt ord() function to find the ASCII value of a character
+    """
+    pass
+
+
+def test_function(test_case):
+    test_string = test_case[0]
+    solution = test_case[1]
+
+    if case_sort(test_string) == solution:
+        print("Pass")
+    else:
+        print("False")
+
+
+test_string = 'fedRTSersUXJ'
+solution = "deeJRSfrsTUX"
+test_case = [test_string, solution]
+test_function(test_case)
+
+
+test_string = "defRTSersUXI"
+solution = "deeIRSfrsTUX"
+test_case = [test_string, solution]
+test_function(test_case)
