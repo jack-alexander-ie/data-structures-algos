3ef1061d2f4f5d62d5ee5c7c6ac66d05d66352df
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-08 08:07:53 -0700

    Starting on sorting algos

---
+++ Topics/3. Basic Algorithms/2. Sorting Algorithms/bubble_sort.py
@@ -0,0 +1,10 @@
+wakeup_times = [16,49,3,12,56,49,55,22,13,46,19,55,46,13,25,56,9,48,45]
+
+
+def bubble_sort_1(l):
+    # TODO: Implement bubble sort solution
+    pass
+
+
+bubble_sort_1(wakeup_times)
+print("Pass" if (wakeup_times[0] == 3) else "Fail")
