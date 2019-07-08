8e9d948d371df39177311dec9cc5ae05b479e882
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-08 15:12:11 -0700

    String merge sort implementation added

--- Topics/3. Basic Algorithms/2. Sorting Algorithms/merge_sort_string_sort.py
+++ Topics/3. Basic Algorithms/2. Sorting Algorithms/merge_sort_string_sort.py
@@ -11,14 +11,41 @@ def case_sort(string):
 
     Note: You can use Python's inbuilt ord() function to find the ASCII value of a character
     """
-    pass
+
+    upper_ch_index = 0
+    lower_ch_index = 0
+
+    sorted_string = sorted(string)
+
+    for index, character in enumerate(sorted_string):
+        # check if character is lower-case
+        ascii_int = ord(character)
+        if 97 <= ascii_int <= 122:  # ASCII value of a = 97 & ASCII value of z = 122
+            lower_ch_index = index
+            break
+
+    output = list()
+    for character in string:
+        ascii_int = ord(character)
+        # if character is lower case pick next lower_case character
+        if 97 <= ascii_int <= 122:
+            output.append(sorted_string[lower_ch_index])
+            lower_ch_index += 1
+        else:
+            output.append(sorted_string[upper_ch_index])
+            upper_ch_index += 1
+    return "".join(output)
 
 
 def test_function(test_case):
     test_string = test_case[0]
     solution = test_case[1]
 
-    if case_sort(test_string) == solution:
+    result = case_sort(test_string)
+
+    print(result)
+
+    if result == solution:
         print("Pass")
     else:
         print("False")
@@ -30,7 +57,7 @@ test_case = [test_string, solution]
 test_function(test_case)
 
 
-test_string = "defRTSersUXI"
-solution = "deeIRSfrsTUX"
-test_case = [test_string, solution]
-test_function(test_case)
+# test_string = "defRTSersUXI"
+# solution = "deeIRSfrsTUX"
+# test_case = [test_string, solution]
+# test_function(test_case)
