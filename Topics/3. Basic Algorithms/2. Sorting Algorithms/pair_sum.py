b60fd858011649915810c36582b4cb24887459d5
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-08 17:09:02 -0700

    test cases added for pair sum implementation

--- Topics/3. Basic Algorithms/2. Sorting Algorithms/pair_sum.py
+++ Topics/3. Basic Algorithms/2. Sorting Algorithms/pair_sum.py
@@ -19,3 +19,33 @@ def pair_sum(arr, target):
             back_index -= 1  # sum > target ==> shift back pointer backward
 
     return [None, None]
+
+
+def test_function(test_case):
+    input_list = test_case[0]
+    target =test_case[1]
+    solution = test_case[2]
+    output = pair_sum(input_list, target)
+    if output == solution:
+        print("Pass")
+    else:
+        print("False")
+
+
+input_list = [2, 7, 11, 15]
+target = 9
+solution = [2, 7]
+test_case = [input_list, target, solution]
+test_function(test_case)
+
+input_list = [0, 8, 5, 7, 9]
+target = 9
+solution = [0, 9]
+test_case = [input_list, target, solution]
+test_function(test_case)
+
+input_list = [110, 9, 89]
+target = 9
+solution = [None, None]
+test_case = [input_list, target, solution]
+test_function(test_case)
