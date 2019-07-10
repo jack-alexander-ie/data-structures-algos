ee84133e2af689a134ec8116828c3ab65bfcbadf
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-10 16:54:53 -0700

    Dutch National Flag implementation added

--- Projects/Project_3/problem_4.py
+++ Projects/Project_3/problem_4.py
@@ -14,51 +14,36 @@ def sort_012(input_list: list):
     Args:
        input_list(list): List to be sorted
     """
-    start, mid, pivot = 0, 0, 1
-    end = len(input_list) - 1
+    # initialize pointers for next positions of 0 and 2
+    next_pos_0 = 0
+    next_pos_2 = len(input_list) - 1
 
-    last_zero_idx, last_one_idx, last_two_idx = 0, 0, 0
+    front_index = 0
 
-    for number in
+    while front_index <= next_pos_2:
+        if input_list[front_index] == 0:
+            input_list[front_index] = input_list[next_pos_0]
+            input_list[next_pos_0] = 0
+            next_pos_0 += 1
+            front_index += 1
+        elif input_list[front_index] == 2:
+            input_list[front_index] = input_list[next_pos_2]
+            input_list[next_pos_2] = 2
+            next_pos_2 -= 1
+        else:
+            front_index += 1
 
-    while mid <= end:
-
-        if input_list[mid] < pivot:     # current element is 0
-
-            # Pop it out and insert it at the front
-            temp = input_list.pop(mid)
-            input_list.insert(0, )
-
-            temp = input_list[start]
-            input_list[start] = input_list[mid]
-            input_list[mid] = temp
-
-            start += 1
-            mid += 1
-
-        elif input_list[mid] > pivot:   # current element is 2
-
-            temp = input_list[mid]
-            input_list[mid] = input_list[end]
-            input_list[end] = temp
-
-            end -= 1
-
-        else:                           # current element is 1
-            mid += 1
-
-    print(input_list)
+    return input_list
 
 
 def test_function(test_case):
     sorted_array = sort_012(test_case)
-    print(sorted_array)
     if sorted_array == sorted(test_case):
         print("Pass")
     else:
         print("Fail")
 
 
-# test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
-# test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
-# test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
