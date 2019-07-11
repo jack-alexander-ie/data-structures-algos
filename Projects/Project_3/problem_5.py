6468d913af0c13624b56ecca11834da1a8b0613d
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-10 17:14:44 -0700

    Minor additions

--- Projects/Project_3/problem_5.py
+++ Projects/Project_3/problem_5.py
@@ -1,35 +1,49 @@
 from ipywidgets import widgets
 from IPython.display import display
 from ipywidgets import interact
+from collections import defaultdict
 
 
 class TrieNode:
     def __init__(self):
         # Initialize this node in the Trie
-        pass
+        self.children = defaultdict(TrieNode)
+        self.is_word = False
 
     def insert(self, char):
         # Add a child node in this Trie
         pass
 
     def suffixes(self, suffix=''):
-        # Recursive function that collects the suffix for
-        # all complete words below this point
+        # Recursive function that collects the suffix for all complete words below this point
         pass
 
 
 class Trie:
     def __init__(self):
         # Initialize this Trie (add a root node)
-        pass
+        self.root = TrieNode()
 
     def insert(self, word):
-        # Add a word to the Trie
-        pass
+        """
+        Add `word` to trie
+        """
+        current_node = self.root
+
+        for char in word:                                    # 1. Assess each character in the inout word
+            current_node = current_node.children[char]       # 2. Update the node to point it to the next node
+            current_node.is_word = True                      # 3. Set the characters end_word bool to true
 
     def find(self, prefix):
         # Find the Trie node that represents this prefix
-        pass
+        current_node = self.root
+
+        for char in word:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+
+        return current_node.is_word
 
 
 MyTrie = Trie()
