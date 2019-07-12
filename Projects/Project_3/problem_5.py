64a2268d5c68674b13a80b01d4bdfc0eac8f467f
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-12 15:49:12 -0700

    Trie class implemented along with TrieNode insert function

--- Projects/Project_3/problem_5.py
+++ Projects/Project_3/problem_5.py
@@ -7,11 +7,22 @@ class TrieNode:
         self.is_word = False
 
     def insert(self, char):
-        # Add a child node in this Trie
-        self.children[char] = TrieNode()
+        """ Adds a child node to a TrieNode """
+        if len(char) > 1:
+            print('Warning: Cannot add string to node - must be single character')
+            return
+        elif len(char) < 1:
+            print('Warning: Cannot add empty character to node')
+            return
+
+        if char in self.children:               # Character is valid, check if it already exists
+            print('Warning: Character already exists - please insert another')
+            return
+        else:
+            self.children[char] = TrieNode()    # Add character if it doesn't exist
 
     def suffixes(self, suffix=''):
-        # Recursive function that collects the suffix for all complete words below this point
+        """ Recursively collects the suffix for all complete words below a TrieNode """
         pass
 
 
@@ -20,26 +31,31 @@ class Trie:
         self.root = TrieNode()
 
     def insert(self, word):
-        """
-        Add a word to the trie
-        """
+        """ Adds a word to the Trie """
         current_node = self.root
+        last_index = len(word) - 1
+        for index, char in enumerate(word):              # 1. Assess each character in the input word
+            current_node = current_node.children[char]   # 2. Update the node to point it to the next node
+            if index is last_index:                      # 3. Set to true if last character is in the word
+                current_node.is_word = True              # 4. Flip char's is_word bool to denote complete word
 
-        for char in word:                                    # 1. Assess each character in the inout word
-            current_node = current_node.children[char]       # 2. Update the node to point it to the next node
-            current_node.is_word = True                      # 3. Set the characters end_word bool to true
+    def exists(self, word):
+        """ Checks if a word exists in a Trie """
+        current_node = self.root
+        for char in word:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+        return current_node.is_word
 
     def find(self, prefix):
-        # Find the Trie node that represents this prefix
-        # current_node = self.root
-        #
-        # for char in word:
-        #     if char not in current_node.children:
-        #         return False
-        #     current_node = current_node.children[char]
-        #
-        # return current_node.is_word
-        pass
+        """ Returns a node node that represents a prefix """
+        current_node = self.root
+        for char in prefix:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+        return current_node
 
 
 MyTrie = Trie()
@@ -50,7 +66,7 @@ word_list = [
     "trie", "trigger", "trigonometry", "tripod"
 ]
 
-print(MyTrie.root.children['a'].children['n'].children['t'].is_word)
-
 for word in word_list:
     MyTrie.insert(word)
+
+print(MyTrie.find('ant').children)
