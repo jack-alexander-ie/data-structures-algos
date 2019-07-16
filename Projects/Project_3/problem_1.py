ea286efef345492b158ea4432b6a81c9644982a7
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-15 19:44:36 -0700

    Added another test case, type hints added and it can only accept integers

--- Projects/Project_3/problem_1.py
+++ Projects/Project_3/problem_1.py
@@ -1,17 +1,14 @@
-def sqrt(number):
-
-    if number < 2:                              # No root, return number itself
+def sqrt(number: int) -> int:
+    if type(number) is not int:
+        print('Warning: Input can only be an integer')
+        return 0
+    if number < 2:                              # No root
         return number                           # Return itself as only square
-
     low, high = 1, number//2                    # Square root cannot be more than number/2
-
     result = 0
-
     while low <= high:
-
         mid_value = (low + high) // 2           # Center value
         mid_squared = mid_value * mid_value
-
         if mid_squared == number:               # Perfect square exists, return value
             return mid_value
         elif mid_squared < number:              # Move right
@@ -19,15 +16,17 @@ def sqrt(number):
             result = mid_value
         else:                                   # Move left
             high = mid_value - 1
-
     return result
 
-# Test Cases - All pass
+# Test Cases - All should pass
 # print("Pass" if (3 == sqrt_rec(9)) else "Fail")
 # print("Pass" if (0 == sqrt_rec(0)) else "Fail")
 # print("Pass" if (4 == sqrt_rec(16)) else "Fail")
 # print("Pass" if (1 == sqrt_rec(1)) else "Fail")
 # print("Pass" if (5 == sqrt_rec(27)) else "Fail")
 
-# Test Case - Large numbers
+# Test Case - Large numbers should pass too
 # print("Pass" if (100 == sqrt(10000)) else "Fail")
+
+# Test Case - Input not an integer
+sqrt('a')   # Output should be 'Warning: Input can only be an integer'
