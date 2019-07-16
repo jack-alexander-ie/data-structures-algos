b544243504a627f3074905369115dc5a45078f10
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-16 15:28:51 -0700

    Made split_path and test_path of router class private

--- Projects/Project_3/problem_7.py
+++ Projects/Project_3/problem_7.py
@@ -48,30 +48,30 @@ class Router:
 
     def __init__(self, root_handler, path_not_found):
         """ Creates a new RouteTrie for holding routes """
-        if self.test_path(root_handler) and self.test_path(path_not_found):
-            self.route_trie = RouteTrie(root_handler)           # Passes root handler to the initialised trie
-            self.path_not_found = path_not_found                # Stores 404 not found response
+        if self.__test_path(root_handler) and self.__test_path(path_not_found):
+            self.route_trie = RouteTrie(root_handler)             # Passes root handler to the initialised trie
+            self.path_not_found = path_not_found                  # Stores 404 not found response
 
     def add_handler(self, path, handler) -> None:
         """ Adds a handler for a path """
-        if self.test_path(path) and self.test_path(handler):
-            path_parts = self.split_path(path)                  # Splits parts into constituent components
-            self.route_trie.insert(path_parts, handler)         # Passes parts on for addition to the trie
+        if self.__test_path(path) and self.__test_path(handler):
+            path_parts = self.__split_path(path)                  # Splits parts into constituent components
+            self.route_trie.insert(path_parts, handler)           # Passes parts on for addition to the trie
 
     def lookup(self, path) -> str:
         """ Lookup path and return its handler """
-        if self.test_path(path):
-            path_parts = self.split_path(path)                  # Splits parts into constituent components
-            handler = self.route_trie.find(path_parts)          # Stores result of path search
-            return handler if handler else self.path_not_found  # Returns handler if there's a match, else 404 error
+        if self.__test_path(path):
+            path_parts = self.__split_path(path)                  # Splits parts into constituent components
+            handler = self.route_trie.find(path_parts)            # Stores result of path search
+            return handler if handler else self.path_not_found    # Returns handler if there's a match, else 404 error
 
     @staticmethod
-    def split_path(path: str) -> List[str]:
+    def __split_path(path: str) -> List[str]:
         """ Splits path into its constituent parts """
         return [part for part in path.split('/') if part]   # Splits path at '/', handles extra slashes in the process
 
     @staticmethod
-    def test_path(path):
+    def __test_path(path):
         if type(path) is not str:
             print('Warning: Input path must be a string')
             exit()
