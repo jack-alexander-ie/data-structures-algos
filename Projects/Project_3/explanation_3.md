4587f8f7e66ef56c4a34b7f2b0095a345ede3991
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-18 16:48:22 -0700

    Added comments to problem_3 and added its explanation

--- Projects/Project_3/explanation_3.md
+++ Projects/Project_3/explanation_3.md
@@ -1,17 +1,31 @@
 # Problem 3 - Rearrange Array Elements
 
-
-
 ### Time Complexity
 
+The function `rearrange_digits` performs several operations which contribute to the overall time complexity of the 
+algorithm. The first part, which tests the values in a list, ensure that only integers are in the list in `O(n)` time.
+
+The second, and most important part, sorts the list using the `merge_sort` function. This breaks the array into `n` 
+number of arrays which are then sorted one by one in `O(n log n)` time.
+
+The third part sorts the input list in descending order so that the maximum value can be found. This is also performed 
+in `O(n)` time.
+
+Finally, the max values are found by repeating the process of popping off one integer and adding it to the first list,
+then adds the next integer to the second list. This is repeated until the original list is empty. It is completed in 
+`O(n)` time as it's time depends on the number of elements in the original input list.
 
 
 ### Space Complexity
 
+The space complexity for this list is `O(n)` for a couple reasons. During the merge sort, the original input array is 
+broken down into arrays containing one of all it's constituent parts, meaning there are at one point `O(n)` arrays.
 
+Similarly, the strings that the integer lists are broken down into are also `O(n)` as they will hold `O(n/2)` elements 
+each.
 
 ## Conclusion
 
-Time Complexity: 
+Time Complexity: `O(n log n)`
 
-Space Complexity: 
+Space Complexity: `O(n)`
--- Projects/Project_3/problem_3.py
+++ Projects/Project_3/problem_3.py
@@ -1,33 +1,42 @@
 from typing import List
 
 
-def merge_sort(arr: List[int]):
-    """ Executes a merge sort to sort the numbers in an array """
+def test_list(list_ints: List[int]) -> bool:
+    """ Checks all elements in a list to ensure they're all integers """
+    for element in list_ints:          # O(n) traversal afforded -> will not add significantly to time complexity
+        if type(element) is not int:
+            print('Warning: One or more of the elements in the list is not an integer')
+            return True
+    return False
+
+
+def merge_sort(arr: List[int]) -> None:
+    """ Performs a merge sort to sort the numbers in an array """
     if len(arr) > 1:
-        mid = len(arr) // 2         # Find the mid of the array
-        left_array = arr[:mid]      # Divide the array elements
-        right_array = arr[mid:]     # into 2 halves
+        mid = len(arr) // 2                                  # Find the mid of the array
+        left_array, right_array = arr[:mid], arr[mid:]       # Divide array elements into 2 halves
 
-        merge_sort(left_array)       # Sort the first half
-        merge_sort(right_array)      # Sort the second half
+        merge_sort(left_array)                               # Sort the first half
+        merge_sort(right_array)                              # Sort the second half
 
-        i = j = k = 0
+        i = j = k = 0                                        # Set tracking variables
 
-        while i < len(left_array) and j < len(right_array):    # Copy data to temp arrays
-            if left_array[i] < right_array[j]:
-                arr[k] = left_array[i]
-                i += 1
+        while i < len(left_array) and j < len(right_array):  # Copy data to temporary arrays
+            if left_array[i] < right_array[j]:               # If left < right element...
+                arr[k] = left_array[i]                       # ...replace kth value in input array
+                i += 1                                       # Move right in temporary left-hand array
             else:
-                arr[k] = right_array[j]
-                j += 1
-            k += 1
+                arr[k] = right_array[j]                      # Right < left, replace kth value in input array
+                j += 1                                       # Move right in temporary right-hand array
+            k += 1                                           # Bump kth index up one
 
-        while i < len(left_array):    # Check if any elements remain
+        # 2 x (1. Check if elements remain, 2. add spares to temp array, 3. increase trackers)
+        while i < len(left_array):
             arr[k] = left_array[i]
             i += 1
             k += 1
 
-        while j < len(right_array):   # Check if any elements remain
+        while j < len(right_array):
             arr[k] = right_array[j]
             j += 1
             k += 1
@@ -35,24 +44,21 @@ def merge_sort(arr: List[int]):
 
 def rearrange_digits(input_list: List[int]) -> List[int]:
     """ Rearranges array elements to form two numbers such that their sum is the maximum """
-    for element in input_list:          # O(n) traversal afforded -> will not add significantly to time complexity
-        if type(element) is not int:
-            print('Warning: One or more of the elements in the list is not an integer')
-            return [0, 0]
-    merge_sort(input_list)              # 1. Sort the input list - O(n log n)
-    reverse_list(input_list)            # 2. Sort list in descending order - O(n)
-    return find_max_pair(input_list)    # 3. Return max pair - O(n)
+    if test_list(input_list): return [0, 0]                  # 1. Test if list values are all ints, return if not
+    merge_sort(input_list)                                   # 2. Merge sort the input list
+    reverse_list(input_list)                                 # 3. Sort list in descending order
+    return find_max_pair(input_list)                         # 4. Return max pair
 
 
-def reverse_list(list_of_nums: List[int]):
-    """ Executes in-place reversal of elements """
+def reverse_list(list_of_nums: List[int]) -> None:
+    """ Reverses elements of list in-place """
     for i in range(len(list_of_nums)):
         number = list_of_nums.pop()
         list_of_nums.insert(i, number)
 
 
 def find_max_pair(input_list: List[int]) -> List[int]:
-    """ Finds and return the pair with the maximum value """
+    """ Finds and returns the pair with the maximum value """
     str_1, str_2 = '', ''
     index = 0
     while index <= len(input_list) + 1:
