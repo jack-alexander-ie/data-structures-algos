76db1481143bd3e9757f7ba0a9723124a6215e8b
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-18 17:34:23 -0700

    Comments added to and explanation added for problem_6

--- Projects/Project_3/explanation_6.md
+++ Projects/Project_3/explanation_6.md
@@ -1,17 +1,16 @@
 # Problem 6 - Max and Min in a Unsorted Array
 
-
-
 ### Time Complexity
 
-
+The time complexity for this algorithm is `O(n)` as it cycles through all the elements in the list just one time.
 
 ### Space Complexity
 
-
+The space complexity for this algorithm is `O(1)` as it simply cycles through the list it was given, and creates only
+a small, fixed number of variables to track the min and max numbers.
 
 ## Conclusion
 
-Time Complexity: 
+Time Complexity: `O(n)`
 
-Space Complexity: 
+Space Complexity: `O(1)`
--- Projects/Project_3/problem_6.py
+++ Projects/Project_3/problem_6.py
@@ -1,5 +1,11 @@
-import random
 from typing import List, Tuple
+
+
+def test_int(number):
+    if type(number) != int:
+        print('Warning: Cannot compare a value that is not an integer')
+        return False
+    return True
 
 
 def get_min_max(ints: List[int]) -> Tuple[int, int]:
@@ -7,11 +13,9 @@ def get_min_max(ints: List[int]) -> Tuple[int, int]:
     if len(ints) < 1:
         print('Warning: Cannot operate on an empty list')
         return 0, 0
-    min_val, max_val = ints[0], 0
-    for number in ints:
-        if type(number) != int:
-            print('Warning: Cannot compare a value that is not an integer')
-            return 0, 0
+    min_val, max_val = ints[0], ints[0]         # Choose two random values from the list
+    for number in ints:                         # Iterate over the list to find min and max
+        if not test_int(number): return 0, 0    # Test each value to make sure it's an integer
         if number >= max_val:
             max_val = number
         if number <= min_val:
@@ -29,5 +33,9 @@ def get_min_max(ints: List[int]) -> Tuple[int, int]:
 # get_min_max(list_of_ints)                       # Expected: 'Warning: Cannot compare a value that is not an integer'
 
 # Test Case - Empty input
-list_of_ints = []
-get_min_max(list_of_ints)                       # Expected: 'Warning: Cannot operate on an empty list'
+# list_of_ints = []
+# get_min_max(list_of_ints)                       # Expected: 'Warning: Cannot operate on an empty list'
+
+# Test Case - two identical numbers
+# list_of_ints = [10, 10]
+# print("Pass" if ((10, 10) == get_min_max(list_of_ints)) else "Fail")     # Expected: Pass
