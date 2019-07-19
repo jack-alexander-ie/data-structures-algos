75dcf07f8f368514a0771636fcce5476dd221b37
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-19 15:25:23 -0700

    Changed min platform name, adding min operations problem

---
+++ Topics/4. Advanced Algorithms/Greedy Algorithms/min_operations_problem.py
@@ -0,0 +1,29 @@
+def min_operations(target):
+    """
+    Return number of steps taken to reach a target number
+    input: target number (as an integer)
+    output: number of steps (as an integer)
+    """
+    return target
+
+
+def test_function(test_case):
+    target = test_case[0]
+    solution = test_case[1]
+    output = min_operations(target)
+
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+target = 18
+solution = 6
+test_case = [target, solution]
+test_function(test_case)
+
+target = 69
+solution = 9
+test_case = [target, solution]
+test_function(test_case)
