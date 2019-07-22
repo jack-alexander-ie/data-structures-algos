0dac29878e0f144ca2061f3632731a99bf9a8e71
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-22 15:27:23 -0700

    Adding udacity implementation

--- Topics/4. Advanced Algorithms/Graph Algorithms/graph_dfs.py
+++ Topics/4. Advanced Algorithms/Graph Algorithms/graph_dfs.py
@@ -49,34 +49,44 @@ graph1.add_edge(nodeS, nodeR)
 
 def dfs_search_test(root: GraphNode, target: any) -> GraphNode:
     """ Executes a depth first search of a graph """
-
     visited_stack = []
-
     current_node = root
-
     while current_node:
-
-        if current_node.value is target:      # Check if current node is target value
+        if current_node.value is target:            # Check if current node is target value
             return current_node
-
         if current_node not in visited_stack:       # Add it to the stack
             visited_stack.append(current_node)
-
         if current_node.children is not []:         # Check if it has children
-
             for child in current_node.children:     # Choose an edge
-
                 if child not in visited_stack:      # Only visit if it's not already in the stack
-
                     if child.value is target:       # Return the node if target found
                         return child
-
                     current_node = child
         else:
-
             current_node = visited_stack.pop()      # Go back to previous node if no children
 
 
-assert nodeA == dfs_search_test(nodeS, 'A')
-assert nodeS == dfs_search_test(nodeP, 'S')
-assert nodeR == dfs_search_test(nodeH, 'R')
+# assert nodeA == dfs_search_test(nodeS, 'A')
+# assert nodeS == dfs_search_test(nodeP, 'S')
+# assert nodeR == dfs_search_test(nodeH, 'R')
+
+
+def dfs_search(root_node, search_value):
+    visited = []
+    stack = [root_node]
+
+    while len(stack) > 0:
+        current_node = stack.pop()
+        visited.append(current_node)
+
+        if current_node.value == search_value:
+            return current_node
+
+        for child in current_node.children:
+            if child not in visited:
+                stack.append(child)
+
+
+assert nodeA == dfs_search(nodeS, 'A')
+assert nodeS == dfs_search(nodeP, 'S')
+assert nodeR == dfs_search(nodeH, 'R')
