18a95e03247d761610d342bfa1e5b5989ae1bb67
Jack Mason <jackmasonmusic92@gmail.com>
2019-07-29 18:29:59 -0700

    Starting work on connecting islands problem

--- Topics/4. Advanced Algorithms/Graph Algorithms/connecting_islands_problem.py
+++ Topics/4. Advanced Algorithms/Graph Algorithms/connecting_islands_problem.py
@@ -8,24 +8,14 @@ def get_minimum_cost_of_connecting(num_islands, bridge_config):
     return: cost (int) minimum cost of connecting all islands
     TODO complete this method to return minimum cost of connecting all islands
     """
+
+    # TODO: Implement Dijkstra's algorithm using a prio queue
+
     pass
-
-
-def test_function(test_case):
-    num_islands = test_case[0]
-    bridge_config = test_case[1]
-    solution = test_case[2]
-    output = get_minimum_cost_of_connecting(num_islands, bridge_config)
-
-    if output == solution:
-        print("Pass")
-    else:
-        print("Fail")
 
 
 num_islands = 4
 bridge_config = [[1, 2, 1], [2, 3, 4], [1, 4, 3], [4, 3, 2], [1, 3, 10]]
-solution = 6
+# solution = 6
 
-test_case = [num_islands, bridge_config, solution]
-test_function(test_case)
+print(get_minimum_cost_of_connecting(num_islands, bridge_config))
