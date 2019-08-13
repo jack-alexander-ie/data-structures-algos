845fd2137be3ab64b15a635e1f95252a5826767b
Jack Mason <jackmasonmusic92@gmail.com>
2019-08-12 22:17:09 -0700

    Last commit for the day - added heuristic function

--- Projects/Project_4/student_code.py
+++ Projects/Project_4/student_code.py
@@ -1,10 +1,24 @@
+from helpers import load_map
+
+map_40 = load_map('map-40.pickle')
+
+
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
 def shortest_path(graph, start, goal):
-    for intersection in graph.intersections.items():
-        print(intersection)
 
-    print('-' * 20)
+    def print_info():
+        for intersection in graph.intersections.items():
+            print(intersection)
+        print('-' * 20)
+        for road in graph.roads:
+            print(road)
 
-    for road in graph.roads:
-        print(road)
+    pass
 
-    return
+
+shortest_path(map_40, 5, 34)
