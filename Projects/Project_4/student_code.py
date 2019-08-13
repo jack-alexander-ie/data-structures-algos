0a7b630d4a0a08985bd590b5ad8d18d7f0e1950d
Jack Mason <jackmasonmusic92@gmail.com>
2019-08-12 19:19:34 -0700

    Adding initial project files

---
+++ Projects/Project_4/student_code.py
@@ -0,0 +1,10 @@
+def shortest_path(graph, start, goal):
+    for intersection in graph.intersections.items():
+        print(intersection)
+
+    print('-' * 20)
+
+    for road in graph.roads:
+        print(road)
+
+    return
